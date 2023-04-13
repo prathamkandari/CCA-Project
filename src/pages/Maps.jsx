ad9e7fa43e8fd9b001297bee35b6581db00ab63a
prathamkandari <prathamkandari123@gmail.com>
2023-04-13 15:55:48 +0530

    Latest

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Component } from "react";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
     render() {
         return (
-            <Map
-                google={this.props.google}
-                style={{ width: '100%', height: '100%' }}
-                zoom={10}
-                initialCenter = {
-                    {
-                        lat: 30.066753,
-                        lng: 79.019302
-                    }
-                }
-            />
-        )
+            <><Map google={this.props.google}
+                style={{ width: '100%%', height: '100%', left: '-8%' }}
+                zoom={10} />
+                <Marker onClick={this.onInfoWindowClick}
+                    name={'Current Location'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </> 
+        );
     }
 }
 
-export default GoogleApiWrapper({
-    apiKey: "AIzaSyAMn_y4o58Lq7cHa9n96dHK591DnG8MA4Y"
+                export default GoogleApiWrapper({
+                    apiKey: "AIzaSyAMn_y4o58Lq7cHa9n96dHK591DnG8MA4Y"
 })(MapContainer)
